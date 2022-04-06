66acb1ed8fedcda32e7c20b046e255229c3cf9dd
kuu7life <kuu7life@gmail.com>
2022-04-06 15:56:54 +0800

    calculator alyout"

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Kutman Amangeldiev on 2022/4/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
